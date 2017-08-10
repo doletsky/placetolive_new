50d3a751c6317552c153559e861839ffe660cc33
adoletskiy <adoletsky@indigos.ru>
2017-08-10 21:19:14 +0300

    debug main page

--- index.php
+++ index.php
@@ -5,153 +5,61 @@ $APPLICATION->SetTitle("Место, в котором мы живем");
     <div class="home-widget home-widget-one shortcodes"><a name="anonces"></a>
         <div class="grid grid-pad">
             <div class="col-1-1">
-
-
-
-
-                <section id="mt-projects">
-
-
-                    <div class="grid grid-pad">
-                        <div class="col-1-1">
-                            <h3 class="widget-title">Новые публикации</h3>
-                        </div>
-                    </div>
-
-
-
-                    <div class="grid grid-pad overflow">
-
-
-
-
-                        <div class="col-1-2 mt-column-clear">
-                            <div class="project-box">
-                                <a href="/project/mercedes-benz/">
-                                    <div class="project-content"><div>
-
-                        <span>
-
-							<h3>Mercedes Benz</h3>
-
-                        	<div class="project-bg" style="background-image: url(<?=SITE_TEMPLATE_PATH?>/images/general.jpg);"></div>
-
-                        </span>
-
-                                        </div></div>
-                                </a>
-                            </div>
-                        </div>
-
-
-                        <div class="col-1-2 mt-column-clear">
-                            <div class="project-box">
-                                <a href="/project/burton/">
-                                    <div class="project-content"><div>
-
-                        <span>
-
-							<h3>Burton</h3>
-
-                        	<div class="project-bg" style="background-image: url(<?=SITE_TEMPLATE_PATH?>/images/burton.jpg);"></div>
-
-                        </span>
-
-                                        </div></div>
-                                </a>
-                            </div>
-                        </div>
-
-
-                        <div class="col-1-2 mt-column-clear">
-                            <div class="project-box">
-                                <a href="/project/banana-republic/">
-                                    <div class="project-content"><div>
-
-                        <span>
-
-							<h3>Banana Republic</h3>
-
-                        	<div class="project-bg" style="background-image: url(<?=SITE_TEMPLATE_PATH?>/images/banana.jpg);"></div>
-
-                        </span>
-
-                                        </div></div>
-                                </a>
-                            </div>
-                        </div>
-
-
-                        <div class="col-1-2 mt-column-clear">
-                            <div class="project-box">
-                                <a href="/project/starbucks-coffee/">
-                                    <div class="project-content"><div>
-
-                        <span>
-
-							<h3>Starbucks Coffee</h3>
-
-                        	<div class="project-bg" style="background-image: url(<?=SITE_TEMPLATE_PATH?>/images/coffee.jpg);"></div>
-
-                        </span>
-
-                                        </div></div>
-                                </a>
-                            </div>
-                        </div>
-
-
-                        <div class="col-1-2 mt-column-clear">
-                            <div class="project-box">
-                                <a href="/project/nikon-cameras/">
-                                    <div class="project-content"><div>
-
-                        <span>
-
-							<h3>Nikon Cameras</h3>
-
-                        	<div class="project-bg" style="background-image: url(<?=SITE_TEMPLATE_PATH?>/images/nikon.jpg);"></div>
-
-                        </span>
-
-                                        </div></div>
-                                </a>
-                            </div>
-                        </div>
-
-
-                        <div class="col-1-2 mt-column-clear">
-                            <div class="project-box">
-                                <a href="/project/burger-king/">
-                                    <div class="project-content"><div>
-
-                        <span>
-
-							<h3>Burger King</h3>
-
-                        	<div class="project-bg" style="background-image: url(<?=SITE_TEMPLATE_PATH?>/images/burger.jpg);"></div>
-
-                        </span>
-
-                                        </div></div>
-                                </a>
-                            </div>
-                        </div>
-
-
-
-                    </div>
-
-
-
-                </section>
-
-
-
-
-
-
-
+                        <?$APPLICATION->IncludeComponent("bitrix:news.list","main_page_pl",Array(
+                                "DISPLAY_DATE" => "Y",
+                                "DISPLAY_NAME" => "Y",
+                                "DISPLAY_PICTURE" => "Y",
+                                "DISPLAY_PREVIEW_TEXT" => "Y",
+                                "AJAX_MODE" => "Y",
+                                "IBLOCK_TYPE" => "news",
+                                "IBLOCK_ID" => "34",
+                                "NEWS_COUNT" => "6",
+                                "SORT_BY1" => "ACTIVE_FROM",
+                                "SORT_ORDER1" => "DESC",
+                                "SORT_BY2" => "SORT",
+                                "SORT_ORDER2" => "ASC",
+                                "FILTER_NAME" => "",
+                                "FIELD_CODE" => Array("ID"),
+                                "PROPERTY_CODE" => Array("DESCRIPTION"),
+                                "CHECK_DATES" => "Y",
+                                "DETAIL_URL" => "",
+                                "PREVIEW_TRUNCATE_LEN" => "",
+                                "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                                "SET_TITLE" => "N",
+                                "SET_BROWSER_TITLE" => "N",
+                                "SET_META_KEYWORDS" => "N",
+                                "SET_META_DESCRIPTION" => "N",
+                                "SET_LAST_MODIFIED" => "Y",
+                                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                                "ADD_SECTIONS_CHAIN" => "N",
+                                "HIDE_LINK_WHEN_NO_DETAIL" => "Y",
+                                "PARENT_SECTION" => "",
+                                "PARENT_SECTION_CODE" => "",
+                                "INCLUDE_SUBSECTIONS" => "Y",
+                                "CACHE_TYPE" => "A",
+                                "CACHE_TIME" => "3600",
+                                "CACHE_FILTER" => "Y",
+                                "CACHE_GROUPS" => "Y",
+                                "DISPLAY_TOP_PAGER" => "Y",
+                                "DISPLAY_BOTTOM_PAGER" => "Y",
+                                "PAGER_TITLE" => "Новости",
+                                "PAGER_SHOW_ALWAYS" => "Y",
+                                "PAGER_TEMPLATE" => "",
+                                "PAGER_DESC_NUMBERING" => "Y",
+                                "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                                "PAGER_SHOW_ALL" => "Y",
+                                "PAGER_BASE_LINK_ENABLE" => "Y",
+                                "SET_STATUS_404" => "Y",
+                                "SHOW_404" => "Y",
+                                "MESSAGE_404" => "",
+                                "PAGER_BASE_LINK" => "",
+                                "PAGER_PARAMS_NAME" => "arrPager",
+                                "AJAX_OPTION_JUMP" => "N",
+                                "AJAX_OPTION_STYLE" => "Y",
+                                "AJAX_OPTION_HISTORY" => "N",
+                                "AJAX_OPTION_ADDITIONAL" => ""
+                            )
+                        );?>
 
 
             </div>
