37611e5ba2e4313e324e87e1c11fbed4f702231c
adoletskiy <adoletsky@indigos.ru>
2017-09-07 00:04:08 +0300

    remove dev to prod

--- bitrix/templates/pl/header.php
+++ bitrix/templates/pl/header.php
@@ -35,7 +35,46 @@ IncludeTemplateLangFile(__FILE__);
     <style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
 
     <script type="text/javascript" src="<?=SITE_TEMPLATE_PATH?>/js/jscripts.js"></script>
-    <script type="text/javascript" async="" src="<?=SITE_TEMPLATE_PATH?>/css/styles3.css"></script></head>
+    <script type="text/javascript" async="" src="<?=SITE_TEMPLATE_PATH?>/css/styles3.css"></script>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter44799454 = new Ya.Metrika({
+                        id:44799454,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true
+                    });
+                } catch(e) { }
+            });
+
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else { f(); }
+        })(document, window, "yandex_metrika_callbacks");
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/44799454" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-100077242-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+</head>
 
 <body class="home page-template page-template-template-home page-template-template-home-php page page-id-352">
 <div id="page" class="hfeed site">
