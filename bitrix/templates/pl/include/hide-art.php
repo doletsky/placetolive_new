cb4c7167d0e90ec055cfa3bd7e8feaf551ddcade
adoletskiy <adoletsky@indigos.ru>
2017-09-08 14:15:38 +0300

    show hide elements in detail for admins

---
+++ bitrix/templates/pl/include/hide-art.php
@@ -0,0 +1,178 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+
+CModule::IncludeModule("iblock");
+$arFilter = Array(
+    "IBLOCK_CODE"=>'articles',
+    "CODE" => $CODE
+);
+$arSelect = array(
+    "ID",
+    "IBLOCK_ID",
+    "NAME",
+    "PREVIEW_PICTURE",
+    "DETAIL_PAGE_URL",
+    "DETAIL_TEXT",
+    "IBLOCK_SECTION_ID",
+    "PROPERTY_RESOURSE"
+);
+$res = CIBlockElement::GetList(Array("created_date"=>"DESC"), $arFilter, false, false, $arSelect);
+if($ar_fields = $res->GetNext())
+{
+
+    $APPLICATION->SetTitle($ar_fields['NAME']);
+    $ar_fields["PREVIEW_PICTURE_SRC"]=CFile::GetPath($ar_fields["PREVIEW_PICTURE"]);
+
+    $resSec = CIBlockSection::GetByID($ar_fields["IBLOCK_SECTION_ID"]);
+    if($arSec = $resSec->GetNext())
+        $ar_fields["SECTION"]=$arSec;
+
+    $VALUES = array();
+    $resFields = CIBlockElement::GetProperty($ar_fields["IBLOCK_ID"], $ar_fields["ID"], "sort", "asc", array("CODE" => "PHOTOS"));
+    while ($ob = $resFields->GetNext())
+    {
+        $VALUES[] = array("SRC" => CFile::GetPath($ob['VALUE']), "DESCRIPTION" => $ob["DESCRIPTION"]);
+    }
+    $ar_fields["PHOTOS"]=$VALUES;
+
+    if($ar_fields["ID"]>507){
+        $nDETAIL_TEXT=explode("\n", strip_tags($ar_fields["DETAIL_TEXT"]));
+        $arSearch=array(
+            '[h]',
+            '[/h]',
+            '[cc]',
+            '[/cc]',
+            '[cl]',
+            '[/cl]',
+            '[cr]',
+            '[/cr]',
+            '[line]',
+            '[/line]',
+            '[row]',
+            '[/row]',
+            '[imgl',
+            '[imgr',
+            '[imgc',
+            '[imgf',
+            '[b]',
+            '[/b]'
+        );
+        $arReplace=array(
+            '<h3>',
+            '</h3>',
+            '<blockquote class="fa fa-quote-left"><p>',
+            '</p></blockquote>',
+            '<blockquote class="fa fa-quote-left alignleft"><p>',
+            '</p></blockquote>',
+            '<blockquote class="fa fa-quote-left alignright"><p>',
+            '</p></blockquote>',
+            '<div class="img-in-line">',
+            '</div>',
+            '<div class="img-in-row">',
+            '</div>',
+            '<div class="alignleft img"><img ',
+            '<div class="alignright img"><img ',
+            '<div class="aligncenter img"><img ',
+            '<div class="full-width-image"><img ',
+            '<em style="font-weight: bold">',
+            '</em>'
+        );
+        foreach($ar_fields['PHOTOS'] as $num=>$pPhoto){
+            $arSearch[]='_'.(1+$num).']';
+            if(strlen($pPhoto['DESCRIPTION'])>2)
+                $arReplace[]='src="'.$pPhoto['SRC'].'" alt="'.$pPhoto['DESCRIPTION'].'"><div class="subtitle-img">'.$pPhoto['DESCRIPTION'].'</div></div>';
+            else
+                $arReplace[]='src="'.$pPhoto['SRC'].'" alt="'.$ar_fields['NAME'].'"></div>';
+        }
+        $arTmp=array();
+        foreach($nDETAIL_TEXT as $str){
+            $tmpStr=str_replace($arSearch,$arReplace,$str,$cntRepl);
+            if($cntRepl==0)$tmpStr="<p>".$tmpStr."</p>";
+            $arTmp[]=$tmpStr;
+        }
+        $ar_fields["DETAIL_TEXT"]=implode($arTmp,'');
+    }
+?>
+    <header class="page-entry-header">
+        <div class="grid grid-pad overflow">
+            <div class="col-1-1">
+                <div class="animated fadeInUp delay">
+                    <h1 class="entry-title"><?$APPLICATION->ShowTitle()?></h1>
+                </div>
+            </div>
+        </div>
+        <div class="page-bg-image" data-parallax="scroll" data-image-src="<?=$ar_fields['PREVIEW_PICTURE_SRC']?>" data-z-index="1">
+        </div>
+    </header><!-- .entry-header -->
+
+    <section id="page-content-container" class="animated fadeIn delay-2">
+        <div class="grid grid-pad page-contain-full">
+            <div class="col-1-1">
+
+                <div id="primary" class="content-area shortcodes">
+                    <main id="main" class="site-main" role="main">
+                        <div class="main-navigation breadcrump">
+                            <ul>
+                                <li><a href="/">Главная</a></li>
+                                <li><a href="<?=$ar_fields['SECTION']['SECTION_PAGE_URL']?>"><?=$ar_fields['SECTION']['NAME']?></a></li>
+                            </ul>
+                        </div>
+
+                        <article id="post-25" class="post-25 page type-page status-publish has-post-thumbnail hentry">
+                            <div class="entry-content">
+                                <?=$ar_fields['DETAIL_TEXT']?>
+                                <?if(strlen($ar_fields['PROPERTY_RESOURSE_VALUE'])):?>
+                                    <p>Источник: <?=$ar_fields['PROPERTY_RESOURSE_VALUE']?></p>
+                                <?endif?>
+                            </div>
+                            <!-- .entry-content -->
+
+                            <div class="social">
+                                <span>Поделиться статьей: </span>
+                                <a target="_blank" href="http://www.facebook.com/sharer.php?u=http://<?=$_SERVER['SERVER_NAME']?><?=$APPLICATION->GetCurPage()?>">
+                                    <i class="fa fa-facebook"></i> </a>
+                                <a target="_blank" href="https://vk.com/share.php?url=http://<?=$_SERVER['SERVER_NAME']?>/<?=$APPLICATION->GetCurPage()?>" >
+                                    <i class="fa fa-vk"></i> </a>
+                            </div>
+                        </article><!-- #post-## -->
+
+
+
+
+
+                    </main><!-- #main -->
+                </div><!-- #primary -->
+            </div>
+        </div>
+    </section>
+    <script>
+        $(document).ready(function(){
+
+            $(".project-bg img").each(function(){
+                console.log($(this).height());
+                console.log($(this).parent().height());
+                if($(this).parent().height()-$(this).height()>1){
+                    console.log('chaging');
+                    $(this).css('max-width','none');
+                    $(this).css('max-height',$(this).parents('.project-content').height()+'px');
+                }
+            });
+            setTimeout(function(){
+                $('.full-width-image').each(function(){
+                    var imgHeight=$(this).children('img').height();
+                    $(this).css('min-height',imgHeight+'px');
+                    $(this).children('.subtitle-img').css('margin-top',imgHeight+'px');
+                    $(this).children('.subtitle-img').css('text-align','center');
+                });
+            }, 300);
+            setTimeout(function(){
+                $('.full-width-image').each(function(){
+                    var imgHeight=$(this).children('img').height();
+                    $(this).css('min-height',imgHeight+'px');
+                    $(this).children('.subtitle-img').css('margin-top',imgHeight+'px');
+                    $(this).children('.subtitle-img').css('text-align','center');
+                });
+            }, 15000);
+        });
+
+    </script>
+<?}?>
--- detail.php
+++ detail.php
@@ -1,70 +1,70 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Title");
+//$APPLICATION->SetTitle("Title");
 ?>
         <!-- POST WITH IMAGE -->
-<?//if(in_array("1", $USER->GetUserGroupArray())){
-//    ?><!--<pre>--><?//print_r($_REQUEST)?><!--</pre>--><?//
-//die();}?>
+<?if(in_array("1", $USER->GetUserGroupArray())){
+    $APPLICATION->IncludeFile(SITE_TEMPLATE_PATH."/include/hide-art.php", array("CODE"=>$_REQUEST["CODE"]));
+}else{
+    $APPLICATION->IncludeComponent("bitrix:news.detail","pl",Array(
+            "DISPLAY_DATE" => "Y",
+            "DISPLAY_NAME" => "Y",
+            "DISPLAY_PICTURE" => "Y",
+            "DISPLAY_PREVIEW_TEXT" => "Y",
+            "USE_SHARE" => "Y",
+            "SHARE_HIDE" => "N",
+            "SHARE_TEMPLATE" => "",
+            "SHARE_HANDLERS" => array("delicious"),
+            "SHARE_SHORTEN_URL_LOGIN" => "",
+            "SHARE_SHORTEN_URL_KEY" => "",
+            "AJAX_MODE" => "N",
+            "IBLOCK_TYPE" => "news",
+            "IBLOCK_ID" => "34",
+            "ELEMENT_ID" => "",
+            "ELEMENT_CODE" => $_REQUEST["CODE"],
+            "CHECK_DATES" => "Y",
+            "FIELD_CODE" => Array("ID", "PREVIEW_PICTURE", "TAGS", "SECTION_CODE"),
+            "PROPERTY_CODE" => Array("RESOURSE", "PHOTOS"),
+            "IBLOCK_URL" => "news.php?ID=#IBLOCK_ID#\"",
+            "DETAIL_URL" => "",
+            "SET_TITLE" => "Y",
+            "SET_CANONICAL_URL" => "Y",
+            "SET_BROWSER_TITLE" => "Y",
+            "BROWSER_TITLE" => "-",
+            "SET_META_KEYWORDS" => "Y",
+            "META_KEYWORDS" => "-",
+            "SET_META_DESCRIPTION" => "Y",
+            "META_DESCRIPTION" => "-",
+            "SET_STATUS_404" => "Y",
+            "SET_LAST_MODIFIED" => "Y",
+            "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+            "ADD_SECTIONS_CHAIN" => "Y",
+            "ADD_ELEMENT_CHAIN" => "N",
+            "ACTIVE_DATE_FORMAT" => "d.m.Y",
+            "USE_PERMISSIONS" => "N",
+            "GROUP_PERMISSIONS" => Array(),
+            "CACHE_TYPE" => "A",
+            "CACHE_TIME" => "3600",
+            "CACHE_GROUPS" => "Y",
+            "DISPLAY_TOP_PAGER" => "Y",
+            "DISPLAY_BOTTOM_PAGER" => "Y",
+            "PAGER_TITLE" => "Страница",
+            "PAGER_TEMPLATE" => "",
+            "PAGER_SHOW_ALL" => "Y",
+            "PAGER_BASE_LINK_ENABLE" => "Y",
+            "SHOW_404" => "Y",
+            "FILE_404" => "/404.php",
+            "MESSAGE_404" => "",
+            "STRICT_SECTION_CHECK" => "Y",
+            "PAGER_BASE_LINK" => "",
+            "PAGER_PARAMS_NAME" => "arrPager",
+            "AJAX_OPTION_JUMP" => "N",
+            "AJAX_OPTION_STYLE" => "N",
+            "AJAX_OPTION_HISTORY" => "N"
+        )
+    );
+}
 
-        <?
-        $APPLICATION->IncludeComponent("bitrix:news.detail","pl",Array(
-                "DISPLAY_DATE" => "Y",
-                "DISPLAY_NAME" => "Y",
-                "DISPLAY_PICTURE" => "Y",
-                "DISPLAY_PREVIEW_TEXT" => "Y",
-                "USE_SHARE" => "Y",
-                "SHARE_HIDE" => "N",
-                "SHARE_TEMPLATE" => "",
-                "SHARE_HANDLERS" => array("delicious"),
-                "SHARE_SHORTEN_URL_LOGIN" => "",
-                "SHARE_SHORTEN_URL_KEY" => "",
-                "AJAX_MODE" => "N",
-                "IBLOCK_TYPE" => "news",
-                "IBLOCK_ID" => "34",
-                "ELEMENT_ID" => "",
-                "ELEMENT_CODE" => $_REQUEST["CODE"],
-                "CHECK_DATES" => "Y",
-                "FIELD_CODE" => Array("ID", "PREVIEW_PICTURE", "TAGS", "SECTION_CODE"),
-                "PROPERTY_CODE" => Array("RESOURSE", "PHOTOS"),
-                "IBLOCK_URL" => "news.php?ID=#IBLOCK_ID#\"",
-                "DETAIL_URL" => "",
-                "SET_TITLE" => "Y",
-                "SET_CANONICAL_URL" => "Y",
-                "SET_BROWSER_TITLE" => "Y",
-                "BROWSER_TITLE" => "-",
-                "SET_META_KEYWORDS" => "Y",
-                "META_KEYWORDS" => "-",
-                "SET_META_DESCRIPTION" => "Y",
-                "META_DESCRIPTION" => "-",
-                "SET_STATUS_404" => "Y",
-                "SET_LAST_MODIFIED" => "Y",
-                "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
-                "ADD_SECTIONS_CHAIN" => "Y",
-                "ADD_ELEMENT_CHAIN" => "N",
-                "ACTIVE_DATE_FORMAT" => "d.m.Y",
-                "USE_PERMISSIONS" => "N",
-                "GROUP_PERMISSIONS" => Array(),
-                "CACHE_TYPE" => "A",
-                "CACHE_TIME" => "3600",
-                "CACHE_GROUPS" => "Y",
-                "DISPLAY_TOP_PAGER" => "Y",
-                "DISPLAY_BOTTOM_PAGER" => "Y",
-                "PAGER_TITLE" => "Страница",
-                "PAGER_TEMPLATE" => "",
-                "PAGER_SHOW_ALL" => "Y",
-                "PAGER_BASE_LINK_ENABLE" => "Y",
-                "SHOW_404" => "Y",
-                "FILE_404" => "/404.php",
-                "MESSAGE_404" => "",
-                "STRICT_SECTION_CHECK" => "Y",
-                "PAGER_BASE_LINK" => "",
-                "PAGER_PARAMS_NAME" => "arrPager",
-                "AJAX_OPTION_JUMP" => "N",
-                "AJAX_OPTION_STYLE" => "N",
-                "AJAX_OPTION_HISTORY" => "N"
-            )
-        );
         ?>
 
 
