039bb6078171653d0796617aba0cfdc5f885dbb3
adoletskiy <adoletsky@indigos.ru>
2017-09-18 11:46:25 +0300

    add conversion google code

--- bitrix/templates/pl/footer.php
+++ bitrix/templates/pl/footer.php
@@ -116,6 +116,24 @@
     });
 
 </script>
+<!-- Google Code for Placetolive Conversion Page -->
+<script type="text/javascript">
+    /* <![CDATA[ */
+    var google_conversion_id = 965705678;
+    var google_conversion_language = "en";
+    var google_conversion_format = "3";
+    var google_conversion_color = "ffffff";
+    var google_conversion_label = "JqAYCPrMn3UQzv-9zAM";
+    var google_remarketing_only = false;
+    /* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+    <div style="display:inline;">
+        <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/965705678/?label=JqAYCPrMn3UQzv-9zAM&amp;guid=ON&amp;script=0"/>
+    </div>
+</noscript>
 
 
 </body></html>
