ab50b7d5eaef4e1e41ce0233e9ef0d8fcd9eaadd
doletsky <doletsky@bk.ru>
2017-10-30 21:21:34 +0300

    debug colors image

--- bitrix/templates/pl/js/preLoadImg.js
+++ bitrix/templates/pl/js/preLoadImg.js
@@ -11,19 +11,27 @@ $(document).ready(function(){
                     var img = document.createElement('img');
                     img.setAttribute('src', $('#img-preview').attr('src'));
                     var hColPallet=$('#img-preview').outerHeight()/5;
+                    var arCPallet=[];
+                    var flag=1;
                     for(var j=1;j<6;j++){
                         var vibrant = new Vibrant(img, 256, j);
                         var swatches = vibrant.swatches();
                         var i=0;
                         $('.colPallet').css('height', hColPallet+'px');
                         for (swatch in swatches)
+
                             if (swatches.hasOwnProperty(swatch) && swatches[swatch]){
+                                flag=1;
+                                for(var k=0;k<arCPallet.length;k++){
+                                    if(arCPallet[k]==swatches[swatch].getHex()) flag=0;
+                                }
+                                if(flag==1){arCPallet[arCPallet.length]=swatches[swatch].getHex();}
                                 $('.pt'+j+' .colPallet:eq('+i+')').css('background-color', swatches[swatch].getHex());
                                 i++;
                                 console.log(swatch, swatches[swatch].getHex());
                             }
                     }
-
+                    console.log(arCPallet);
 
                 }
                 reader.readAsDataURL(input.files[0]);
