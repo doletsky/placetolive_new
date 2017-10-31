7bcb4bc1c883e1f3579c5de215f3951b37464455
adoletskiy <adoletsky@indigos.ru>
2017-11-01 01:09:11 +0300

    debug page palett in img

--- bitrix/templates/pl/js/preLoadImg.js
+++ bitrix/templates/pl/js/preLoadImg.js
@@ -10,11 +10,13 @@ $(document).ready(function(){
                 reader.onloadend = function (e) {
                     var img = document.createElement('img');
                     img.setAttribute('src', $('#img-preview').attr('src'));
+                    $(".pt1").html('');
+                    $(".form_cpallet").css("height",(110+$('#img-preview').outerHeight())+"px")
                     var hColPallet=$('#img-preview').outerHeight()/5;
                     var arCPallet=[];
                     var flag=1;
                     for(var j=1;j<6;j++){
-                        var vibrant = new Vibrant(img, 256, j);
+                        var vibrant = new Vibrant(img, 128, j);
                         var swatches = vibrant.swatches();
                         var i=0;
                         $('.colPallet').css('height', hColPallet+'px');
@@ -23,15 +25,42 @@ $(document).ready(function(){
                             if (swatches.hasOwnProperty(swatch) && swatches[swatch]){
                                 flag=1;
                                 for(var k=0;k<arCPallet.length;k++){
-                                    if(arCPallet[k]==swatches[swatch].getHex()) flag=0;
+                                    var rgb=swatches[swatch].getRgb();
+                                    if(arCPallet.length>0 && Math.abs(arCPallet[k][1][0]-rgb[0])<10 && Math.abs(arCPallet[k][1][1]-rgb[1])<10 && Math.abs(arCPallet[k][1][2]-rgb[2])<10) flag=0;
                                 }
-                                if(flag==1){arCPallet[arCPallet.length]=swatches[swatch].getHex();}
-                                $('.pt'+j+' .colPallet:eq('+i+')').css('background-color', swatches[swatch].getHex());
+                                if(flag==1){
+                                    arCPallet[arCPallet.length]=[swatches[swatch].getHex(),swatches[swatch].getRgb()];
+                                }
+//                                $('.pt'+j+' .colPallet:eq('+i+')').css('background-color', swatches[swatch].getHex());
                                 i++;
-                                console.log(swatch, swatches[swatch].getRgb());
                             }
                     }
-                    console.log(arCPallet);
+
+                    var bColPallet=0;// колич. увеличивыаемых элементов
+                    var wPt1=Math.ceil(arCPallet.length/5)*100;
+                    var ost=arCPallet.length/Math.ceil(arCPallet.length/5)-Math.ceil(arCPallet.length/Math.ceil(arCPallet.length/5));
+                    console.log(Math.ceil(arCPallet.length/5)*5-arCPallet.length);
+                    if(ost==0 ){
+                        hColPallet=$('#img-preview').outerHeight()/(arCPallet.length/Math.ceil(arCPallet.length/5));
+                    }
+                    else{
+                        if(Math.ceil(arCPallet.length/5)*5-arCPallet.length > Math.ceil(arCPallet.length/5)){
+                            hColPallet=$('#img-preview').outerHeight()/4;
+                            bColPallet=Math.ceil(arCPallet.length/5)*5 - arCPallet.length - Math.ceil(arCPallet.length/5);
+                        }
+                        else bColPallet=Math.ceil(arCPallet.length/5)*5 - arCPallet.length;
+                    }
+                    $(".main_cpallet").css("width",(600+wPt1)+"px");
+                    $(".main_cpallet").css("margin-left",((600+wPt1)/-2)+"px");
+                    $(".pt1").css("width",wPt1+"px");
+                    for(color in arCPallet){
+                        $(".pt1").append("<div class='colPallet' style='background-color:"+arCPallet[color][0]+"'></div>");
+                    }
+                    $('.colPallet').css('height', hColPallet+'px');
+                    for(var n=0; n<bColPallet; n++){
+                        var l=Math.ceil(arCPallet.length/5)+n;
+                        $('.colPallet:eq('+l+')').css('height', 2*hColPallet+'px');
+                    }
 
                 }
                 reader.readAsDataURL(input.files[0]);
--- colors_image/index.php
+++ colors_image/index.php
@@ -3,7 +3,15 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH."/js/Vibrant.js");
 $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH."/js/preLoadImg.js");
+require_once($_SERVER["DOCUMENT_ROOT"].SITE_TEMPLATE_PATH."/include/Mobile_Detect.php");
+$detect = new Mobile_Detect;$PC=1;
+if($detect->isMobile() || $detect->isTablet()) $PC=0;
 ?>
+<style>
+    .colPallet{
+        width: 100px;height: 10px;float: left;
+    }
+</style>
     <header class="page-entry-header">
         <div class="grid grid-pad overflow">
             <div class="col-1-1">
@@ -15,51 +23,48 @@ $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH."/js/preLoadImg.js");
         <div class="page-bg-image" data-parallax="scroll" data-image-src="<?=SITE_TEMPLATE_PATH."/images/krug_23.jpg"?>" data-z-index="1">
         </div>
     </header>
-    <form id="form" action="" method="post" enctype="multipart/form-data">
+<section id="page-content-container" class="animated fadeIn delay-2">
+    <div class="grid grid-pad page-contain-full">
+    <div class="col-1-1">
+
+    <div id="primary" class="content-area shortcodes">
+    <main id="main" class="site-main" role="main">
+    <div class="main-navigation breadcrump">
+        <ul>
+            <li><a href="/">Главная</a></li>
+        </ul>
+    </div>
+<article id="post-25" class="post-25 page type-page status-publish has-post-thumbnail hentry">
+    <div class="entry-content first-box">
+    <p>
+<!--        Хроматичеcкий круг — это онлайн сервис подбора цветовой гаммы для интерьера, вариантов сочетания цветов. Для общего представления--><?//if($PC==0):?><!-- в полноэкранной версии--><?//endif;?><!-- мы предложили эскизный вид, который позволит увидеть цельную картину сочетания цветов, подобрать наиболее подходящий.-->
+    </p>
+    <?if($PC!=1):?>
+        <p class="noMobScr">Страница получения палитры из картинки временно доступна только на устройствах с большим разрешением.</p>
+    <?endif?>
+    <form id="form" action="" method="post" enctype="multipart/form-data" style="position: relative" class="form_cpallet">
         <div>
             <input type="file" id="img" multiple accept="image/*" name="img"/>
         </div>
-        <div style="width: 1110px;">
+        <div class="main_cpallet" style="width: 1100px;position: absolute;left: 50%">
             <img id="img-preview" style="width: 600px;float: left" />
-            <div class="pt1" style="width: 100px;height: 50px;float: left;margin-right: 1px">
-                <div id="pallet1" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet2" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet3" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet4" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet5" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
+            <div class="pt1" style="height: 50px;float: left;">
             </div>
-            <div class="pt2" style="width: 100px;height: 50px;float: left;margin-right: 1px">
-                <div id="pallet1" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet2" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet3" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet4" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet5" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-            </div>
-            <div class="pt3" style="width: 100px;height: 50px;float: left;margin-right: 1px">
-                <div id="pallet1" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet2" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet3" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet4" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet5" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-            </div>
-            <div class="pt4" style="width: 100px;height: 50px;float: left;margin-right: 1px">
-                <div id="pallet1" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet2" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet3" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet4" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet5" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-            </div>
-            <div class="pt5" style="width: 100px;height: 50px;float: left;margin-right: 1px">
-                <div id="pallet1" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet2" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet3" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet4" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-                <div id="pallet5" class="colPallet" style="width: 100px;height: 10px;float: left"></div>
-            </div>
-
             <div style="clear: both"></div>
             <br />
             <a href="#" id="reset-img-preview">удалить изображения</a>
         </div>
     </form>
+    </div>
+    <!-- .entry-content -->
+
+</article>
+        <?$APPLICATION->IncludeFile(SITE_TEMPLATE_PATH."/include/rand-art.php", array("REQUIRED_ID"=>"531"));
+        $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH."/js/detail_script.js");?>
+    </main><!-- #main -->
+    </div><!-- #primary -->
+    </div>
+    </div>
+</section>
+
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
